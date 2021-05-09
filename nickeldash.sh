d043e3d4b8fc6e928063daf1babb5511395b368a
Tom McLaughlin <tom.mclaughlin@objectpartners.com>
2021-05-08 21:36:22 -0500

    separate metrics

--- nickeldash.sh
+++ nickeldash.sh
@@ -15,7 +15,7 @@ catch() {
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "$SCRIPT_DIR"
-xargs -0 aws cloudwatch get-metric-widget-image --metric-widget < metric.json \
+xargs -0 aws cloudwatch get-metric-widget-image --metric-widget < prod-failures.json \
     | jq -r .MetricWidgetImage \
     | base64 --decode \
     | convert -transparent white -background none png:- -rotate -16 -resize 60% png:- \
@@ -23,6 +23,6 @@ xargs -0 aws cloudwatch get-metric-widget-image --metric-widget < metric.json \
     | convert png:- -fill white -undercolor '#00000080' -gravity South -annotate +0+5 " $(date) " composite.png
 
 
-aws s3 cp index.html s3://dev-a-cdn.smart-square.com/nickeldash/ --expires "$(date --date 'now + 1 minute' +%FT%T%z)"
+# aws s3 cp index.html s3://dev-a-cdn.smart-square.com/nickeldash/ --expires "$(date --date 'now + 1 minute' +%FT%T%z)"
 aws s3 cp composite.png s3://dev-a-cdn.smart-square.com/nickeldash/ --expires "$(date --date 'now + 1 minute' +%FT%T%z)"
 
