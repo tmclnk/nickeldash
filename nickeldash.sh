735731cf33d108461761fa0dec1b03ecbdc756fd
Tom McLaughlin <tom.mclaughlin@objectpartners.com>
2021-05-11 12:25:16 -0500

    params

--- nickeldash.sh
+++ nickeldash.sh
@@ -2,6 +2,7 @@
 set -eu
 set -o pipefail
 
+BUCKET=${BUCKET:-mybucket}
 ################################################################################
 # Error Handling
 ################################################################################
@@ -20,9 +21,11 @@ xargs -0 aws cloudwatch get-metric-widget-image --metric-widget < prod-failures.
     | base64 --decode \
     | convert -transparent white -background none png:- -rotate -16 -resize 60% png:- \
     | composite -compose over -size 100x100 -geometry +740+305 png:- lookatthis.png png:- \
-    | convert png:- -fill white -undercolor '#00000080' -gravity South -annotate +0+5 " $(date) " composite.png
+    | convert png:- -fill white -undercolor '#00000080' -gravity South -annotate +0+5 " $(date) " png:- \
+    | convert png:- -fill white -undercolor '#00000080' -font Courier-Bold -pointsize 60 -gravity South -annotate +0+30 "LOOK AT THIS ERROR GRAPH" composite.png
 
 
-# aws s3 cp index.html s3://dev-a-cdn.smart-square.com/nickeldash/ --expires "$(date --date 'now + 1 minute' +%FT%T%z)"
-aws s3 cp composite.png s3://dev-a-cdn.smart-square.com/nickeldash/ --expires "$(date --date 'now + 1 minute' +%FT%T%z)"
+# | convert png:- -scale 50%  composite.png
+aws s3 cp index.html s3://$BUCKET/nickeldash/ --expires "$(date --date 'now + 1 minute' +%FT%T%z)"
+aws s3 cp composite.png s3://$BUCKET/nickeldash/ --expires "$(date --date 'now + 4 minute' +%FT%T%z)"
 
